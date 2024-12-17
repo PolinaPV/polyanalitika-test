98944d4762870eaf5bf7052ccf11c65e8501acbb
Lycoris <podsipanina@gmail.com>
2024-12-17 13:27:50 +0300

    added chif_fio column and fixed sorting by max_overcharge_percent

--- ETL_Query.sql
+++ ETL_Query.sql
@@ -25,7 +25,8 @@ BEGIN
 		s.sale_date,
 		s.salesman_id,
 		s.item_id
-	    FROM sales s ORDER BY s.sale_date, s.salesman_id);
+	    FROM sales s 
+		ORDER BY s.sale_date, s.salesman_id);
   END IF;
   IF $1 = 'month' THEN
     RETURN QUERY (
@@ -40,19 +41,20 @@ BEGIN
 		s.sale_date,
 		s.salesman_id,
 		s.item_id
-		FROM sales s ORDER BY s.sale_date, s.salesman_id);
+		FROM sales s 
+		ORDER BY s.sale_date, s.salesman_id);
   END IF;
 END;
 $$
 LANGUAGE plpgsql;
 
-WITH salesman_with_department AS(
+WITH salesman_with_department AS (
 	-- Добавление данных руководителей к данным продавцов
 	SELECT *
 	FROM salesman sm
 		JOIN department dep ON sm.department_id = dep.department_id
 ),
-all_merch AS(
+all_merch AS (
 	-- Объединение товаров и услуг в одну таблицу
 	SELECT * FROM product
 	UNION
@@ -66,36 +68,36 @@ all_periods AS (
 )
 -- -------Final Query------- --
 SELECT DISTINCT ON (start_date, end_date, swd.fio)
-	-- all_periods.sale_date,
 	period_type,
 	start_date,
 	end_date,
 	swd.fio,
-	-- В Исходных данных нет ФИО руководителя, поэтому выводится id
-	swd.dep_chif_id,
+	sm.fio AS chif_fio,
 	SUM(s.quantity) OVER(PARTITION BY 
 		CONCAT(period_type,start_date, end_date, swd.fio)) AS sales_count,
 	SUM(s.final_price) OVER(PARTITION BY 
 		CONCAT(period_type,start_date, end_date, swd.fio)) AS sales_sum,
-	all_periods.item_id,
-	all_merch.name,
-	MAX((s.final_price / (s.quantity * all_merch.price)::float - 1) * 100
+	ap.item_id,
+	am.name,
+	MAX((s.final_price / (s.quantity * am.price)::float - 1) * 100
 		)::numeric(10, 2) AS max_overcharge_percent,
-	MAX(s.final_price - s.quantity * all_merch.price) AS max_overcharge
-FROM all_periods
-	JOIN all_merch ON all_merch.id = all_periods.item_id
-	JOIN salesman_with_department swd ON all_periods.salesman_id = swd.id
-	JOIN sales s ON all_periods.sale_date = s.sale_date
-		AND all_periods.salesman_id = s.salesman_id 
-		AND all_periods.item_id  = s.item_id
-WHERE s.sale_date BETWEEN all_merch.sdate AND all_merch.edate
+	MAX(s.final_price - s.quantity * am.price) AS max_overcharge
+FROM all_periods ap
+	JOIN all_merch am ON am.id = ap.item_id
+	JOIN salesman_with_department swd ON ap.salesman_id = swd.id
+	JOIN salesman sm ON swd.dep_chif_id = sm.id
+	JOIN sales s ON ap.sale_date = s.sale_date
+		AND ap.salesman_id = s.salesman_id 
+		AND ap.item_id  = s.item_id
+WHERE s.sale_date BETWEEN am.sdate AND am.edate
 GROUP BY period_type,
 	start_date,
 	end_date,
 	swd.fio,
-	all_periods.item_id,
-	all_merch.name,
+	sm.fio,
+	ap.item_id,
+	am.name,
 	s.quantity,
 	s.final_price,
 	swd.dep_chif_id
-ORDER BY swd.fio, start_date 
+ORDER BY swd.fio, start_date, end_date, max_overcharge_percent DESC
